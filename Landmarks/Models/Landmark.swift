67cb4e37f8cf4d6fae7a0bde0332da1750002d8e
laurelr2020 <reganlaurell@gmail.com>
2019-10-13 14:32:24 -0400

    section 1: setup and rename

--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -12,33 +12,32 @@ import CoreLocation
 struct Landmark: Hashable, Codable {
     var id: Int
     var name: String
-    
     fileprivate var imageName: String
     fileprivate var coordinates: Coordinates
-    
-    var city: String
     var state: String
-    var country: String
+    var park: String
     var category: Category
-    
-    var locationCoordinate: CLLocationCoordinate2D{
-        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-    
+
     enum Category: String, CaseIterable, Codable, Hashable {
         case featured = "Featured"
-        case parks = "Parks"
-        case museums = "Musumes"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
     }
 }
 
 extension Landmark {
-    var image: Image{
+    var image: Image {
         ImageStore.shared.image(name: imageName)
     }
 }
 
-struct Coordinates: Hashable, Codeable {
+struct Coordinates: Hashable, Codable {
     var latitude: Double
     var longitude: Double
 }
